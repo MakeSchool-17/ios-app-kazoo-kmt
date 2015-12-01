c089af3588375e725e62b14a752b13c814f545fe
Kazu Komoto <kazuhiro.komoto@students.makeschool.com>
2015-12-01 11:43:11 -0800

    Initialized the project

---
+++ DozeDrivingPrevention/Detector.mm
@@ -0,0 +1,136 @@
+//
+//  Detector.m
+//  DozeDrivingPrevention
+//
+//  Created by 小林和宏 on 12/1/15.
+//  Copyright © 2015 mycompany. All rights reserved.
+//
+
+#import "DozeDrivingPrevention-Bridging-Header.h"
+#import <opencv2/opencv.hpp>
+#import <opencv2/highgui/ios.h>
+
+@interface Detector()
+{
+    cv::CascadeClassifier face_cascade;
+    cv::CascadeClassifier eyes_cascade;
+}
+@end
+
+@implementation Detector: NSObject
+
+- (id)init {
+    self = [super init];
+    
+    // 分類器の読み込み
+    NSBundle *face_bundle = [NSBundle mainBundle];
+    //    NSString *face_path = [face_bundle pathForResource:@"lbpcascade_frontalface" ofType:@"xml"];
+    NSString *face_path = [face_bundle pathForResource:@"haarcascade_frontalface_alt" ofType:@"xml"];
+    std::string face_cascadeName = (char *)[face_path UTF8String];
+    
+    if(!face_cascade.load(face_cascadeName)) {
+        return nil;
+    }
+    
+    // Add cascade for eyes
+    NSBundle *eyes_bundle = [NSBundle mainBundle];
+    NSString *eyes_path = [eyes_bundle pathForResource:@"haarcascade_eye_tree_eyeglasses" ofType:@"xml"];
+    //    NSString *eyes_path = [eyes_bundle pathForResource:@"haarcascade_eye" ofType:@"xml"];
+    //    NSString *eyes_path = [eyes_bundle pathForResource:@"haarcascade_lefteye_2splits" ofType:@"xml"];
+    std::string eyes_cascadeName = (char *)[eyes_path UTF8String];
+    
+    if(!eyes_cascade.load(eyes_cascadeName)) {
+        return nil;
+    }
+    
+    return self;
+    
+}
+
+- (UIImage *)recognizeFace:(UIImage *)image {
+    // UIImage -> cv::Mat変換
+    CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
+    CGFloat cols = image.size.width;
+    CGFloat rows = image.size.height;
+    
+    cv::Mat mat(rows, cols, CV_8UC4); // 8 bits per component, 4 channels (color channels + alpha)
+    CGContextRef contextRef = CGBitmapContextCreate(mat.data, // Pointer to data
+                                                    cols, // Width of bitmap
+                                                    rows, // height of bitmap
+                                                    8, // Bits per component
+                                                    mat.step[0], // Bytes per row
+                                                    colorSpace, // Colorspace
+                                                    kCGImageAlphaNoneSkipLast |
+                                                    kCGBitmapByteOrderDefault); // Bitmap info flags
+    
+    CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
+    CGContextRelease(contextRef);
+    
+    // 顔検出（画像,出力短形,縮小スケール,最低短形数,フラグ？,最小短形）
+    std::vector<cv::Rect> faces; // Rect is defined by position of rectungle and length of row and column
+    face_cascade.detectMultiScale(mat, faces,
+                                  1.1, 2,
+                                  CV_HAAR_SCALE_IMAGE,
+                                  cv::Size(60, 60));
+    //                                cv::Size(30, 30));
+    
+    // FIXME
+    std::cout << "size of faces: " << faces.size() << std::endl;
+    
+    
+    // 顔の位置に丸を描く（結果の描画）
+    std::vector<cv::Rect>::const_iterator r = faces.begin();
+    for(; r != faces.end(); ++r) {
+        cv::Point face_center;
+        int face_radius;
+        face_center.x = cv::saturate_cast<int>((r->x + r->width*0.5));
+        face_center.y = cv::saturate_cast<int>((r->y + r->height*0.5));
+        face_radius = cv::saturate_cast<int>((r->width + r->height) / 2);
+        cv::circle(mat, face_center, face_radius, cv::Scalar(80,80,255), 3, 8, 0 );
+        
+        // FIXME
+        std::cout << "faces rect: " << *r << std::endl;
+        
+        // In each face, detect eyes
+        cv::Mat faceROI = mat(*r);
+        
+        /*
+         // Cut lower parts in face
+         r->height = r->height * 0.3;
+         */ // When changing the factor, you need to change const_iterator to interator
+        
+        std::vector<cv::Rect> eyes;
+        eyes_cascade.detectMultiScale(faceROI, eyes,
+                                      1.1, 3,
+                                      CV_HAAR_SCALE_IMAGE,
+                                      cv::Size(30,30));
+        //                                    cv::Size(10,10));
+        
+        // FIXME
+        std::cout << "size of eyes: " << eyes.size() << std::endl;
+        std::cout << eyes.empty() << std::endl; // Detect eyes are open or not
+        
+        // draw eyes' positions
+        std::vector<cv::Rect>::const_iterator nr = eyes.begin(); // n means nested
+        for(; nr != eyes.end(); ++nr) {
+            cv::Point eyes_center;
+            int eyes_radius;
+            eyes_center.x = cv::saturate_cast<int>((r->x + nr->x + nr->width*0.5));
+            eyes_center.y = cv::saturate_cast<int>((r->y + nr->y + nr->height*0.5));
+            eyes_radius = cv::saturate_cast<int>((nr->width + nr->height) / 2);
+            cv::circle(mat, eyes_center, eyes_radius, cv::Scalar(80,255,80), 3, 8, 0 ); // last three parameters are Tichness, Linetype and NoShift (each shift value devides the rectangle size and location by 2)
+            
+            // FIXME
+            std::cout << "eyes rect: " << *nr << std::endl;
+            
+        }
+        
+    }
+    
+    // cv::Mat -> UIImage変換
+    UIImage *resultImage = MatToUIImage(mat);
+    
+    return resultImage;
+}
+
+@end
---
+++ DozeDrivingPrevention/ViewController.swift
@@ -0,0 +1,141 @@
+//
+//  ViewController.swift
+//  DozeDrivingPrevention
+//
+//  Created by 小林和宏 on 12/1/15.
+//  Copyright © 2015 mycompany. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    // セッション
+    var mySession : AVCaptureSession!
+    // カメラデバイス
+    var myDevice : AVCaptureDevice!
+    // 出力先
+    var myOutput : AVCaptureVideoDataOutput!
+    
+    // 顔検出オブジェクト
+    let detector = Detector()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // カメラを準備
+        if initCamera() {
+            // 撮影開始
+            mySession.startRunning()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // カメラの準備処理
+    func initCamera() -> Bool {
+        // セッションの作成.
+        mySession = AVCaptureSession()
+        
+        // 解像度の指定.
+        mySession.sessionPreset = AVCaptureSessionPresetMedium
+        
+        
+        // デバイス一覧の取得.
+        let devices = AVCaptureDevice.devices()
+        
+        // バックカメラをmyDeviceに格納.
+        for device in devices {
+            if(device.position == AVCaptureDevicePosition.Front){
+                //                if(device.position == AVCaptureDevicePosition.Back){
+                myDevice = device as! AVCaptureDevice
+            }
+        }
+        if myDevice == nil {
+            return false
+        }
+        
+        // バックカメラからVideoInputを取得.
+        var myInput: AVCaptureDeviceInput! = nil
+        do {
+            myInput = try AVCaptureDeviceInput(device: myDevice) as AVCaptureDeviceInput
+        } catch let error {
+            print(error)
+        }
+        
+        // セッションに追加.
+        if mySession.canAddInput(myInput) {
+            mySession.addInput(myInput)
+        } else {
+            return false
+        }
+        
+        // 出力先を設定
+        myOutput = AVCaptureVideoDataOutput()
+        myOutput.videoSettings = [ kCVPixelBufferPixelFormatTypeKey: Int(kCVPixelFormatType_32BGRA) ]
+        
+        
+        
+        // FPSを設定
+        do {
+            try myDevice.lockForConfiguration()
+            
+            myDevice.activeVideoMinFrameDuration = CMTimeMake(1, 15)
+            myDevice.unlockForConfiguration()
+        } catch let error {
+            print("lock error: \(error)")
+            return false
+        }
+        
+        // デリゲートを設定
+        let queue: dispatch_queue_t = dispatch_queue_create("myqueue",  nil)
+        myOutput.setSampleBufferDelegate(self, queue: queue)
+        
+        
+        // 遅れてきたフレームは無視する
+        myOutput.alwaysDiscardsLateVideoFrames = true
+        
+        // セッションに追加.
+        if mySession.canAddOutput(myOutput) {
+            mySession.addOutput(myOutput)
+        } else {
+            return false
+        }
+        
+        // カメラの向きを合わせる
+        for connection in myOutput.connections {
+            if let conn = connection as? AVCaptureConnection {
+                if conn.supportsVideoOrientation {
+                    conn.videoOrientation = AVCaptureVideoOrientation.Portrait
+                }
+            }
+        }
+        
+        return true
+    }
+    
+    
+    // 毎フレーム実行される処理
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!)
+    {
+        dispatch_sync(dispatch_get_main_queue(), {
+            // UIImageへ変換
+            let image = CameraUtil.imageFromSampleBuffer(sampleBuffer)
+            
+            // 顔認識
+            let faceImage = self.detector.recognizeFace(image)
+            
+            // 表示
+            self.imageView.image = faceImage
+        })
+    }
+    
+    
+}
